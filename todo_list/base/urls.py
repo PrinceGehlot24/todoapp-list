088cc34e8455ca4bf5c81cfe89d8d7e9a8bc75a9
PrinceGehlot24 <princegehlot24@gmail.com>
2023-09-01 16:48:24 +0530

    add taskview, taskdetail in the views

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import views
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path('', views.taskList, name='tasks'),
+    path('', TaskList.as_view() , name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
 ]
