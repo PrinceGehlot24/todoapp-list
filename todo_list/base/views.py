c9d9c29cc9934f2093b206b201bf54a88691c2c2
PrinceGehlot24 <princegehlot24@gmail.com>
2023-09-01 17:51:06 +0530

    add login and task delete views

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 from .models import Task
+
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authencticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     model = Task
@@ -15,5 +25,17 @@ class TaskDetail(DetailView):
 
 class TaskCreate(CreateView):
     model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
 
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    template_name = "base/task_delete.html"
 
